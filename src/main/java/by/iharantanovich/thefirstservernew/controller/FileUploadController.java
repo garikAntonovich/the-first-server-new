4e3e74ed484010af75b5476d1829feccf5ce189f
garikAntonovich <garik.antonovich@gmail.com>
2022-02-06 01:43:20 +0300

    the upload status page has been added + minor changes

--- src/main/java/by/iharantanovich/thefirstservernew/controller/FileUploadController.java
+++ src/main/java/by/iharantanovich/thefirstservernew/controller/FileUploadController.java
@@ -1,13 +1,27 @@
 package by.iharantanovich.thefirstservernew.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.multipart.MultipartFile;
 
 @Controller
 public class FileUploadController {
 
     @GetMapping("/upload")
-    private String uploadPage() {
+    private String showUploadPage() {
         return "upload_view";
+    }
+
+    @PostMapping("upload")
+    private String uploadFile(@RequestParam("file") MultipartFile file, Model model) {
+        if (file.isEmpty()) {
+            model.addAttribute("fileIsEmpty", "You have not selected a file... Try again");
+        } else {
+            model.addAttribute("successful", "Successfully uploaded file: " + file.getOriginalFilename() + "!");
+        }
+        return "upload_status_view";
     }
 }
