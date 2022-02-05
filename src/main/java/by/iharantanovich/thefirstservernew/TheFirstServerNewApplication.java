ed3098abc1cf141a494f938d21393118ef47a69d
garikAntonovich <garik.antonovich@gmail.com>
2022-02-06 00:24:42 +0300

    init project

---
+++ src/main/java/by/iharantanovich/thefirstservernew/TheFirstServerNewApplication.java
@@ -0,0 +1,13 @@
+package by.iharantanovich.thefirstservernew;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheFirstServerNewApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TheFirstServerNewApplication.class, args);
+    }
+
+}
---
+++ src/test/java/by/iharantanovich/thefirstservernew/TheFirstServerNewApplicationTests.java
@@ -0,0 +1,13 @@
+package by.iharantanovich.thefirstservernew;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TheFirstServerNewApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
