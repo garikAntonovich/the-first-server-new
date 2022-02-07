5dd33e36944e45d0cfbdba5b079c2f15dc52b27a
garikAntonovich <garik.antonovich@gmail.com>
2022-02-07 22:23:02 +0300

    ExtractingFilesService has been injected in controller

--- src/main/java/by/iharantanovich/thefirstservernew/controller/FileUploadController.java
+++ src/main/java/by/iharantanovich/thefirstservernew/controller/FileUploadController.java
@@ -1,5 +1,7 @@
 package by.iharantanovich.thefirstservernew.controller;
 
+import by.iharantanovich.thefirstservernew.service.ExtractingFilesService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -10,17 +12,25 @@ import org.springframework.web.multipart.MultipartFile;
 @Controller
 public class FileUploadController {
 
+    private final ExtractingFilesService extractingFilesService;
+
+    @Autowired
+    public FileUploadController(ExtractingFilesService extractingFilesService) {
+        this.extractingFilesService = extractingFilesService;
+    }
+
     @GetMapping("/upload")
     private String showUploadPage() {
         return "upload_view";
     }
 
-    @PostMapping("upload")
+    @PostMapping("/upload")
     private String uploadFile(@RequestParam("file") MultipartFile file, Model model) {
         if (file.isEmpty()) {
             model.addAttribute("fileIsEmpty", "You have not selected a file... Try again");
         } else {
             model.addAttribute("successful", "Successfully uploaded file: " + file.getOriginalFilename() + "!");
+            extractingFilesService.extractFiles(file);
         }
         return "upload_status_view";
     }
