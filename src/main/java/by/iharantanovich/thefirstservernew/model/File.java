c3917249c89382c3b042f4e8c2d02bd2d58b6c0b
garikAntonovich <garik.antonovich@gmail.com>
2022-02-07 22:12:35 +0300

    file model has been added

---
+++ src/main/java/by/iharantanovich/thefirstservernew/model/File.java
@@ -0,0 +1,35 @@
+package by.iharantanovich.thefirstservernew.model;
+
+public class File {
+
+    private String name;
+    private String data;
+
+    public File(String name, String data) {
+        this.name = name;
+        this.data = data;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getData() {
+        return data;
+    }
+
+    public void setData(String data) {
+        this.data = data;
+    }
+
+    @Override
+    public String toString() {
+        return "File: " +
+                "name: '" + name + '\'' +
+                ", data: '" + data + '\'';
+    }
+}
